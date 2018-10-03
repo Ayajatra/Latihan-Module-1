847e56f6883710a6499d17a09fa0f33d99a3ef20
Hansen Artajaya <balebodoh@gmail.com>
2018-10-03 15:22:32 +0700

    Add project files.

---
+++ Latihan Module 1/Connection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Latihan_Module_1
+{
+    public static class Connection
+    {
+        // Connection String
+        static string connectionString =
+            "Server=localhost;" +
+            "Database=Session1;" +
+            "Integrated Security=True";
+
+        public static void OpenConnection()
+        {
+            MessageBox.Show("Connecting to SQL Server");
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    MessageBox.Show("Connected");
+                }
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+        }
+    }
+}
